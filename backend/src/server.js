91ce05a34e157c2b9c306e807d11e841c5121b60
Diarapak <krobpierre0@gmail.com>
2025-04-16 12:27:14 +0200

    git fix

---
+++ backend/src/server.js
@@ -0,0 +1,51 @@
+import http from 'http';
+import app from './app.js';
+import config from './config/config.js';
+import { initDatabase } from './config/database.js';
+
+console.log('🚀 Starting server...');
+
+let server;
+
+const startServer = async () => {
+	try {
+		// Connexion à la base SQLite
+		await initDatabase();
+
+		// Lancement du serveur
+		server = app.listen(config.PORT, config.HOST, () => {
+			console.log(`✅ Server running at port :${config.PORT}`);
+		});
+	} catch (err) {
+		console.error('❌ Failed to start server:', err);
+		process.exit(1);
+	}
+};
+
+startServer();
+
+const exitHandler = () => {
+	if (server) {
+		server.close(() => {
+			console.log('🛑 Server closed');
+			process.exit(1);
+		});
+	} else {
+		process.exit(1);
+	}
+};
+
+const unexpectedErrorHandler = (err) => {
+	console.error('💥 Unexpected error:', err);
+	exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+	console.info('📴 SIGTERM received');
+	if (server) {
+		server.close();
+	}
+});
